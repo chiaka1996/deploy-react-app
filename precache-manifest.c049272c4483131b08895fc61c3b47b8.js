93ffa24b176f84baa2ec2e1d8fbecffcc71bfa9c
newton <chiakajunior@yahoo.com>
2020-04-15 03:00:57 +0100

    Updates

---
+++ precache-manifest.c049272c4483131b08895fc61c3b47b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8418124fa6e598156cddcbeef9919a0",
+    "url": "/deploy-react-app/index.html"
+  },
+  {
+    "revision": "032d0ad3454d8a845f1a",
+    "url": "/deploy-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b451c962521998ca2b46",
+    "url": "/deploy-react-app/static/js/2.38f1524e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-react-app/static/js/2.38f1524e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "032d0ad3454d8a845f1a",
+    "url": "/deploy-react-app/static/js/main.a76fdbf8.chunk.js"
+  },
+  {
+    "revision": "95b55f1ad408ddae140d",
+    "url": "/deploy-react-app/static/js/runtime-main.66563c3d.js"
+  }
+]);
